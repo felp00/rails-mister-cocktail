63bac22f5766c011517ab495da79fb5e6dac1403
Felipe Rocha <felipewp200@gmail.com>
2019-05-30 17:20:55 -0300

    cocktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @cocktail = set_doses
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @dose.cocktail = set_doses
+    @dose.save
+
+    redirect_to cocktail_path(set_doses)
+  end
+
+  def destroy
+    @cocktail = Dose.find(params[:id]).cocktail
+    Dose.find(params[:id]).destroy
+
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def set_doses
+    Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
