ce66fe85624f4474efbe169e8cb4b302fabfdd0b
Felipe Rocha <felipewp200@gmail.com>
2019-05-31 18:05:45 -0300

    cocktail_update

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -29,6 +29,6 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :dose)
+    params.require(:cocktail).permit(:name, :dose, :photo)
   end
 end
